a21b4007c230da9ab0a44841cce5d1e8264ca4dd
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-07-05 10:40:41 +0200

    fix bug in repository

--- src/main/java/de/thkoeln/archilab/coalbaseresources/resources/WebLinkResourceRepository.java
+++ src/main/java/de/thkoeln/archilab/coalbaseresources/resources/WebLinkResourceRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.UUID;
 
 @RepositoryRestResource()
 public interface WebLinkResourceRepository extends MongoRepository<WebLinkResource, String> {
 
-  public List<WebLinkResource> findByReferenceId(String referenceId);
+  public List<WebLinkResource> findByReferenceId(UUID referenceId);
 
 }
