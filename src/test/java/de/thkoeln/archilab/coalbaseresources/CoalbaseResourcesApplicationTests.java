692c12c91c2ddf189b33f6b8d0cf8269c3311fc8
JannIntveen <JannIntveen@gmail.com>
2019-08-07 15:06:03 +0200

    Added Embedded Kafka to Test

--- src/test/java/de/thkoeln/archilab/coalbaseresources/CoalbaseResourcesApplicationTests.java
+++ src/test/java/de/thkoeln/archilab/coalbaseresources/CoalbaseResourcesApplicationTests.java
@@ -2,15 +2,20 @@ package de.thkoeln.archilab.coalbaseresources;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@EmbeddedKafka
 public class CoalbaseResourcesApplicationTests {
+
+  @Autowired
+  private EmbeddedKafkaBroker embeddedKafka;
 
   @Test
   public void contextLoads() {
   }
-
 }
